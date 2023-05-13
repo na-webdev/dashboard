ba902f82ceb0542f194d6c042aba23af09f44e9d
Abdurahim <nurmatov.abdurahim98@mail.ru>
2023-05-13 21:13:48 +0500

    completed layout

--- src/app/dashboard/components/statistic-boards/statistic-boards.component.ts
+++ src/app/dashboard/components/statistic-boards/statistic-boards.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-statistic-boards',
   templateUrl: './statistic-boards.component.html',
-  styleUrls: ['./statistic-boards.component.scss']
+  styleUrls: ['./statistic-boards.component.scss'],
 })
 export class StatisticBoardsComponent {
+  staticCards = [
+    { number: 20, type: 'due tasks', completed: 13, title: 'summary' },
+    { number: 20, type: 'tasks', completed: 9, title: 'overdue' },
+    { number: 20, type: 'open', completed: 19, title: 'issues' },
+    { number: 20, type: 'proposals', completed: 16, title: 'features' },
+  ];
 
+  staticCards2 = [
+    { number: 214, type: 'new issues' },
+    { number: 75, type: 'closed' },
+    { number: 3, type: 'fixed' },
+    { number: 4, type: "won't fix" },
+    { number: 8, type: 'reopened' },
+    { number: 6, type: 'needs triage' },
+  ];
 }
--- src/app/shared/components/stat-card/stat-card.component.ts
+++ src/app/shared/components/stat-card/stat-card.component.ts
@@ -1,18 +1,37 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-stat-card',
   templateUrl: './stat-card.component.html',
   styleUrls: ['./stat-card.component.scss'],
 })
-export class StatCardComponent {
+export class StatCardComponent implements OnInit {
   @Input() title: string;
   @Input() statData: string | number;
-  @Input() statType: string;
+  @Input() statType: 'due tasks' | 'tasks' | 'open' | 'proposals' | string;
   @Input() footerContent: string;
   @Input() cardClass = '';
   @Input() mainTextClass = '';
   @Input() subTextClass = '';
 
   @Output() onMoreVertClick = new EventEmitter<string>();
+
+  ngOnInit() {
+    let textColorClass = '';
+    switch (this.statType) {
+      case 'tasks':
+        textColorClass = 'text-red-500';
+        break;
+      case 'open':
+        textColorClass = 'text-orange-500';
+        break;
+      case 'due tasks':
+        textColorClass = 'text-blue-500';
+        break;
+      case 'proposals':
+        textColorClass = 'text-green-500';
+        break;
+    }
+    this.mainTextClass += ' ' + textColorClass;
+  }
 }
